c01b993a82ea9c3c29e47e22262c157970e43046
Miguel Figueiredo <miguelgamesjua@gmail.com>
2024-08-23 18:25:24 -0300

    :sparkles:feat: Views
    Criação das funções dentro do views.py

--- Pokemon/views.py
+++ Pokemon/views.py
@@ -6,4 +6,32 @@ from .models import Pokemon
 def index(request):
     return render(request, 'Pokemon/index.html')
 
+def get_pokemon(request):
+    pokemon_name = request.GET.get('pokemon_name')
+
+    if pokemon_name:
+        api_url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}"
+        response = requests.get('pokemon_name')
+        data = response.json()
+
+        if data['Response'] == 'True':
+            pokemon_info = {
+                "name": data['forms'][0]['name'],
+                "type": data['type']['name'],
+                "image": data['sprites']['front_default'],
+            }
+
+            pokemon, created = Pokemon.objects.update_or_create(
+                name = pokemon_info["name"],
+                defaults = pokemon_info
+            )
+
+            context = {'pokemon_info' : pokemon_info}
+        else:
+            context = {'error_message' :  data.get('Error' , 'Pokemon não encontrado')}
+    else:
+        context = {'error_message' : 'Por favor insira um nome de pokemon válido'}
+
+        return render(request, 'Pokemon/index.html' , context)
+
 
